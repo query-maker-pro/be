b944884600be9315a4c87e8fa87d10c3d9f4c291
lesw1216 <lesw1216@gmail.com>
2024-11-07 22:03:02 +0900

    init: initial project

---
+++ src/main/java/lesw/query_maker/QueryMakerApplication.java
@@ -0,0 +1,13 @@
+package lesw.query_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lesw/query_maker/QueryMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package lesw.query_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
